8b4b08fc242bf94a2d33c9145f6ea9ca2363fe67
MariaP <mprokhorova@gmail.com>
2022-09-05 00:44:36 -0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Client;
+using SP = Microsoft.SharePoint.Client;
+
+using System.Security;
+
+
+
+
+namespace SharepointTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sitePath = "http://samscnsoft.sharepoint.com/";
+
+           // SPSite site = new SPSite(sitePath);
+
+
+
+            string siteUrl = "https://samscnsoft.sharepoint.com/sites/testSite2";
+
+
+            
+            
+
+            ClientContext clientContext = new ClientContext(siteUrl);
+            clientContext.AuthenticationMode = ClientAuthenticationMode.Default;
+
+            clientContext.Credentials = new SharePointOnlineCredentials("mproharava@samscnsoft.onmicrosoft.com", Utility.ConvertToSecureString("Prhd7700llsd"));
+
+            Web oWebsite = clientContext.Web;
+            ListCollection collList = oWebsite.Lists;
+
+            clientContext.Load(collList);
+
+            clientContext.ExecuteQuery();
+
+            foreach (SP.List oList in collList)
+            {
+                Console.WriteLine("Title: {0} Created: {1}", oList.Title, oList.Created.ToString());
+            }
+
+            //SPSite siteCollection = new SPSite(sitePath);
+            //SPWeb site = siteCollection.RootWeb;
+
+            //foreach (SPList list in site.Lists)
+            //{
+            //    Console.WriteLine(list.Title);
+            //}
+
+            //site.Dispose();
+            //siteCollection.Dispose();
+
+            Console.ReadLine();
+        }
+
+        
+    }
+    
+}
---
+++ Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace SharepointTestApp
+{
+    public class Utility
+    {
+        public static SecureString ConvertToSecureString(string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            var securePassword = new SecureString();
+
+            foreach (char c in password)
+                securePassword.AppendChar(c);
+
+            securePassword.MakeReadOnly();
+            return securePassword;
+        }
+    }
+}
